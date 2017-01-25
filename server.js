11eb18e7a7616b948cf261419c055e2d8155d20e
Mengzhen <gao.mengzhen94@gmail.com>
2017-01-25 17:49:47 -0500

    time convertor done

--- server.js
+++ server.js
@@ -1,12 +1,57 @@
-
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+function unix2natural(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 
+  'July', 'August', 'September', 'October', 'November', 'December'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var time = month + ' ' + date + ',' + ' ' + year;
+  return time;
+}
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+/*function natural2unix(natural_time){
+  var time = (+Date.parse(date)) / 1000
+  return time;
+}
+*/
+
+app.get('/:times', function(req, res){
+	var date = req.params.times
+	console.log("date: " + date)
+	var unix = null;
+	var natural = null;
+	if(+date >= 0){
+		natural = unix2natural(date)
+		unix = +date;
+	}
+	if(Date.parse(date) === Date.parse(date)){
+		natural = date
+		unix = (+Date.parse(date)) / 1000
+	}
+	var results = {
+		"unix" : unix,
+		"natural" : natural
+	}
+	res.send(JSON.stringify(results))
 })
+app.listen(8080, function() {
+    console.log("Timestamp App listening on port "+ 8080);
+});
+
+
+
+
+	
+
+
+
+
+
+
+
+ 
+
 
